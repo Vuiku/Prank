d70d96956c1ef1a53454f29ebbf768111d280278
Vuiku <etoyafud@gmail.com>
2025-03-21 15:59:13 +0300

    ADD NEW FILES AND EDIT LAST FILES

---
+++ main.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+from itertools import count
+
+class ImageLabel(tk.Label):
+    """a label that displays images, and plays them if they are gifs"""
+    def load(self, im):
+        if isinstance(im, str):
+            im = Image.open(im)
+        self.loc = 0
+        self.frames = []
+        try:
+            for i in count(1):
+                self.frames.append(ImageTk.PhotoImage(im.copy()))
+                im.seek(i)
+        except EOFError:
+            pass
+        try:
+            self.delay = im.info['duration']
+        except:
+            self.delay = 100
+        if len(self.frames) == 1:
+            self.config(image=self.frames[0])
+        else:
+            self.next_frame()
+    def unload(self):
+        self.config(image="")
+        self.frames = None
+    def next_frame(self):
+        if self.frames:
+            self.loc += 1
+            self.loc %= len(self.frames)
+            self.config(image=self.frames[self.loc])
+            self.after(self.delay, self.next_frame)
+root = tk.Tk()
+root.geometry("1920x1080")
+root.resizable(False, False)
+root.attributes("-fullscreen", True)
+root.title
+
+lbl = ImageLabel(root)
+lbl.pack()
+lbl.load('9fb093a2e2eb672924baeb5bdf3ca549.gif')
+root.mainloop()
