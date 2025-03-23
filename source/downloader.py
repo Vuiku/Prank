1f64472814ee48ab4b0f3beffc60a241c82468aa
Vuiku <etoyafud@gmail.com>
2025-03-23 22:48:18 +0300

    downloader.py

--- source/downloader.py
+++ source/downloader.py
@@ -1,1 +1,5 @@
 import os
+
+os.system("curl -o sound.mp3 https://github.com/Vuiku/Prank/blob/main/sound.mp3")
+os.system("curl -o gif.gif https://github.com/Vuiku/Prank/blob/main/gif.gif")
+os.system("curl -o main.exe https://github.com/Vuiku/Prank/blob/main/main.exe")
